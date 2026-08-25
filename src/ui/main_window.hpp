22f21a08576de2b4f645953c50915787d6c4ac18
Alexandre Vialle <77194804+ppyne@users.noreply.github.com>
2026-08-25 03:37:43 +0200

    Connect Qt layer controls to the document model

--- src/ui/main_window.hpp
+++ src/ui/main_window.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "rosettelab/curves/polar_rose.hpp"
+#include "rosettelab/document/document.hpp"
 
 #include <QMainWindow>
 
 class QDoubleSpinBox;
+class QListWidget;
 class QSpinBox;
 
 namespace rosettelab::ui {
@@ -16,6 +18,10 @@ public:
     explicit MainWindow(QWidget* parent = nullptr);
 
 private:
+    void add_polar_rose();
+    void select_layer(document::LayerId id);
+    void load_active_layer();
+    void sync_layer_order();
     void update_preview();
 
     PreviewWidget* preview_{};
@@ -25,6 +31,9 @@ private:
     QDoubleSpinBox* rotation_{};
     QDoubleSpinBox* tolerance_{};
     QSpinBox* zoom_{};
+    QListWidget* layers_{};
+    document::Document document_;
+    document::LayerId active_layer_id_{};
 };
 
 } // namespace rosettelab::ui
