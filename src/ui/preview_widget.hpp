b7f06aeb48bbf8aec6866137e6fb57f2d0358930
Alexandre Vialle <77194804+ppyne@users.noreply.github.com>
2026-08-25 03:16:41 +0200

    Add fixed document canvas and independent zoom

--- src/ui/preview_widget.hpp
+++ src/ui/preview_widget.hpp
@@ -11,13 +11,16 @@ public:
     explicit PreviewWidget(QWidget* parent = nullptr);
 
     void set_parameters(const curves::PolarRoseParameters& parameters);
+    void set_zoom_percent(double zoom_percent);
 
 protected:
     void paintEvent(QPaintEvent* event) override;
 
 private:
     curves::PolarRoseParameters parameters_;
+    double zoom_percent_{100.0};
+    double page_width_{210.0};
+    double page_height_{210.0};
 };
 
 } // namespace rosettelab::ui
-
