b2f2b05e11642657ed3b4e4b94382178e86c2264
Alexandre Vialle <77194804+ppyne@users.noreply.github.com>
2026-08-25 04:22:51 +0200

    Add synchronized RGBA and HSLA color editor

---
+++ src/ui/color_editor_dialog.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <QColor>
+#include <QDialog>
+
+#include <array>
+
+class QComboBox;
+class QLabel;
+class QPushButton;
+class QSpinBox;
+
+namespace rosettelab::ui {
+
+class ColorEditorDialog final : public QDialog {
+public:
+    ColorEditorDialog(QColor initial_color, QString title, QWidget* parent = nullptr);
+
+    [[nodiscard]] QColor color() const noexcept { return color_; }
+
+private:
+    enum class Format {
+        Rgba,
+        Hsla,
+    };
+
+    void configure_fields();
+    void update_color_from_fields();
+    void update_fields_from_color();
+    void update_preview();
+    void choose_visually();
+
+    QColor color_;
+    bool updating_{false};
+    QComboBox* format_{};
+    std::array<QLabel*, 4> channel_labels_{};
+    std::array<QSpinBox*, 4> channels_{};
+    QPushButton* preview_{};
+};
+
+} // namespace rosettelab::ui
+
