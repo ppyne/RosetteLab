e829d578ae420fbe7d74b6466555b7f48ad3a285
Alexandre Vialle <77194804+ppyne@users.noreply.github.com>
2026-08-25 02:42:23 +0200

    Add Qt 6 live polar rose preview

---
+++ src/ui/main_window.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "rosettelab/curves/polar_rose.hpp"
+
+#include <QMainWindow>
+
+class QDoubleSpinBox;
+class QSpinBox;
+
+namespace rosettelab::ui {
+
+class PreviewWidget;
+
+class MainWindow final : public QMainWindow {
+public:
+    explicit MainWindow(QWidget* parent = nullptr);
+
+private:
+    void update_preview();
+
+    PreviewWidget* preview_{};
+    QDoubleSpinBox* radius_{};
+    QDoubleSpinBox* k_{};
+    QDoubleSpinBox* phase_{};
+    QDoubleSpinBox* rotation_{};
+    QSpinBox* samples_{};
+};
+
+} // namespace rosettelab::ui
+
